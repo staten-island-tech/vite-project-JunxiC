c7737edb284a6b165833cd456639fc1e0d926a02
JunxiC <chenjon09@gmail.com>
2022-12-13 10:16:46 -0500

    vite commit 9

--- vite-project/js/DOM.js
+++ vite-project/js/DOM.js
@@ -1,2 +1,10 @@
-import { menu } from "./menu.js";
-console.log(menu);
+const DOMselectors = {
+  Stocked: document.getElementById("Stocked"),
+  Cheap: document.getElementById("Cheap"),
+  HighRated: document.getElementById("HighRated"),
+  Special: document.getElementById("Special"),
+  HotPie: document.getElementById("HotPie"),
+  ColdPie: document.getElementById("ColdPie"),
+  box: document.getElementById("box"),
+};
+export { DOMselectors };
--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -2,5 +2,6 @@ import "../styles/style.css";
 
 import AOS from "aos";
 import "aos/dist/aos.css"; // You can also use <link> for styles
+import { menu } from "./menu";
 // ..
 AOS.init();
